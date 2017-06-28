cc29c59fb0a0caf7c306b415a1e2c429b6e58b0e
luckyXYY <1353750762@qq.com>
2017-06-29 00:14:45 +0800

    实体类2

---
+++ app/src/main/java/com/example/administrator/bean/Qe.java
@@ -0,0 +1,102 @@
+package com.example.administrator.bean;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class Qe {
+
+    /**
+     * id : 57
+     * question : 这个标志是何含义？
+     * answer : 4
+     * item1 : 40米减速行驶路段
+     * item2 : 最低时速40公里
+     * item3 : 最高时速40公里
+     * item4 : 解除时速40公里限制
+     * explains : 解除时速40公里限制：表示限制速度路段结束。此标志设在限制车辆速度路段的重点。
+     * url : http://images.juheapi.com/jztk/c1c2subject1/57.jpg
+     */
+
+    private int id;
+    private String question;
+    private String answer;
+    private String item1;
+    private String item2;
+    private String item3;
+    private String item4;
+    private String explains;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getItem1() {
+        return item1;
+    }
+
+    public void setItem1(String item1) {
+        this.item1 = item1;
+    }
+
+    public String getItem2() {
+        return item2;
+    }
+
+    public void setItem2(String item2) {
+        this.item2 = item2;
+    }
+
+    public String getItem3() {
+        return item3;
+    }
+
+    public void setItem3(String item3) {
+        this.item3 = item3;
+    }
+
+    public String getItem4() {
+        return item4;
+    }
+
+    public void setItem4(String item4) {
+        this.item4 = item4;
+    }
+
+    public String getExplains() {
+        return explains;
+    }
+
+    public void setExplains(String explains) {
+        this.explains = explains;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
