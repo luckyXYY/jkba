4a79f9f0501dd92c1531b095384cf44c179c0a72
luckyXYY <1353750762@qq.com>
2017-06-29 00:17:40 +0800

    实体类2

---
+++ app/src/main/java/com/example/administrator/bean/Question3.java
@@ -0,0 +1,102 @@
+package com.example.administrator.bean;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class Question3 {
+
+    /**
+     * id : 94
+     * question : 指示标线的作用是什么？
+     * answer : 2
+     * item1 : 禁止通行
+     * item2 : 指示通行
+     * item3 : 限制通行
+     * item4 : 警告提醒
+     * explains : 指示标线主要就是引导你正确的行驶，安全守法
+     * url :
+     */
+
+    private int id;
+    private String question;
+    private String answer;
+    private String item1;
+    private String item2;
+    private String item3;
+    private String item4;
+    private String explains;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getItem1() {
+        return item1;
+    }
+
+    public void setItem1(String item1) {
+        this.item1 = item1;
+    }
+
+    public String getItem2() {
+        return item2;
+    }
+
+    public void setItem2(String item2) {
+        this.item2 = item2;
+    }
+
+    public String getItem3() {
+        return item3;
+    }
+
+    public void setItem3(String item3) {
+        this.item3 = item3;
+    }
+
+    public String getItem4() {
+        return item4;
+    }
+
+    public void setItem4(String item4) {
+        this.item4 = item4;
+    }
+
+    public String getExplains() {
+        return explains;
+    }
+
+    public void setExplains(String explains) {
+        this.explains = explains;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
