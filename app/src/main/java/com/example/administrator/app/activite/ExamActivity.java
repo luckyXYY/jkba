7d22ed43fefbfb5640059bbc5e0addf1f4041623
luckyXYY <1353750762@qq.com>
2017-06-29 16:57:57 +0800

    考试界面

---
+++ app/src/main/java/com/example/administrator/app/activite/ExamActivity.java
@@ -0,0 +1,20 @@
+package com.example.administrator.app.activite;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.app.R;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class ExamActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
