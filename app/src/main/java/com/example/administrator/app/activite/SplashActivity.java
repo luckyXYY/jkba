3428b1317dbbaa7e4abc8856ba7c06c2a2badc87
luckyXYY <1353750762@qq.com>
2017-06-29 10:14:56 +0800

    闪屏界面，主界面的制作

--- app/src/main/java/com/example/administrator/app/activite/SplashActivity.java
+++ app/src/main/java/com/example/administrator/app/activite/SplashActivity.java
@@ -1,10 +1,12 @@
-package com.example.administrator.app;
+package com.example.administrator.app.activite;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.app.R;
 
 /**
  * Created by Administrator on 2017/6/28.
