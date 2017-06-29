c11af0a8df24bbfd064a63059dcabe56da195f60
luckyXYY <1353750762@qq.com>
2017-06-29 23:28:10 +0800

    设置布局中的测试数据为tools类型

--- app/src/main/java/com/example/administrator/app/activite/MainActivity.java
+++ app/src/main/java/com/example/administrator/app/activite/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.administrator.app.activite;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.administrator.app.R;
+import com.example.administrator.bean.ExamInfo;
+import com.example.administrator.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        OkHttpUtils<ExamInfo> utils=new OkHttpUtils<>(getApplication());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo\n";
+        utils.url(uri)
+                .targetClass(ExamInfo.class)
+                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>()
+                {
+                    @Override
+                    public void onSuccess(ExamInfo result) {
+                        Log.e("main","result="+result);
+                    }
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+
+                    }
+                });
         startActivity(new Intent(MainActivity.this,ExamActivity.class));
     }
     public void exit(View view) {
