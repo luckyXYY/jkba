27c33804af2aa9b01637218041c076436eb620d9
luckyXYY <1353750762@qq.com>
2017-06-29 21:26:00 +0800

    点击随机测试，进入考试界面

--- app/src/main/java/com/example/administrator/app/activite/MainActivity.java
+++ app/src/main/java/com/example/administrator/app/activite/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.app.activite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,5 +16,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+    public void exit(View view) {
+        finish();
     }
 }
